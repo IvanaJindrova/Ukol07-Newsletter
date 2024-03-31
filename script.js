e6f676bb57b6ef0a8c9f661e956dc4ec876d2497
Ivana Jindrová <ivajindrova@gmail.com>
2024-03-31 11:08:37 +0200

    Dokončení úkolu

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 const formElm = document.querySelector('#newsletter')
+formElm.style.padding = '2rem'
+formElm.style.backgroundColor = '#f0f0f0'
 
-formElm.addEventListener('submit', () => {
-    //formElm.style.display = 'none'
-    //document.body.innerHTML += `<p>Úspěšně jste se přihlásili k odběru e-mailů na adresu: ${email}. Děkujeme!</p>`
-    //formElm.value.add = 'Děkujeme za Váš zájem, nyní Vás budeme každý týden bombardovat s nabídkami, které nikdy nevyužijete :)'
+const textPole = document.querySelector('#email')
+
+
+textPole.addEventListener('input', (event) => {
+    const vyplnenyEmail = textPole.value
+    const maZavinac = vyplnenyEmail.includes('@')
+    console.log(vyplnenyEmail)
+
+    //Zkouším, zda  i jde udělat podmínka na to, zda zavináč v emailu je nebo není
+    if(maZavinac === false) {
+        textPole.classList.add('red-border')
+        console.log("Funguju na zavináč")
+    }
+
+    //zde chci spojit podmínku na to, že je prázdné pole a zároveň neobsahuje zavináč
+    if(vyplnenyEmail === '' || !maZavinac) {
+        textPole.classList.add('red-border')
+        console.log("Funguju")
+    }
+})
+
+
+formElm.addEventListener('submit', (event) => {
+    event.preventDefault()
     formElm.textContent = 'Děkujeme za Váš zájem, nyní Vás budeme každý týden bombardovat s nabídkami, které nikdy nevyužijete :)'
- // alert('Děkujeme za Váš zájem, nyní Vás budeme každý týden bombardovat s nabídkami, které nikdy nevyužijete :)');
 })
 
